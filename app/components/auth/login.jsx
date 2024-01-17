78f33be5248a59389b8a1c9efe091772474b77b1
jayson7 <lexxiijoo70@gmail.com>
2024-01-17 16:05:05 +0100

    editing

--- app/components/auth/login.jsx
+++ app/components/auth/login.jsx
@@ -4,13 +4,18 @@ import {
   StyleSheet,
   View,
   Text,
+  Image,
   TextInput,
   TouchableOpacity,
 } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
 
+//
 const image = require("../../assets/2.jpg");
-
+//
 export default function Login() {
+  const navigation = useNavigation();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -23,36 +28,53 @@ export default function Login() {
 
   return (
     <ImageBackground source={image} style={styles.container}>
-      {/* ########  logo section */}
-      <View style={styles.logoLogin}></View>
-
-      {/* ends */}
       <View style={styles.containerContent}>
+        {/* ########  logo section */}
+        <View style={styles.logoLogin}></View>
+        <Image
+          style={styles.Logo}
+          source={{
+            uri: "https://res.cloudinary.com/jaytech/image/upload/v1704910436/Beauty_App_prwcqb.png",
+          }}
+        />
+        {/* ends */}
         <Text style={styles.heading}>Login</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Username"
-          onChangeText={(text) => setUsername(text)}
-          value={username}
-          placeholderTextColor="white"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          placeholderTextColor="white"
-        />
+        {/* Username input with icon */}
+        <View style={styles.inputContainer}>
+          <Icon name="user" size={20} color="gold" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            placeholderTextColor="gold"
+            onChangeText={(text) => setUsername(text)}
+            value={username}
+          />
+        </View>
+        {/* Password input with icon */}
+        <View style={styles.inputContainer}>
+          <Icon name="lock" size={20} color="gold" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="gold"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+        </View>
         <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
       </View>
       {/* #################### user exist */}
       <View style={styles.userExist}>
-        <Text style={styles.loginText}>
-          Already have an account? Log in here
-        </Text>
+        <Text style={styles.loginText}>Dont have an account?</Text>
+        <TouchableOpacity
+          style={styles.signupTextContainer}
+          onPress={() => navigation.navigate("register")}
+        >
+          <Text style={styles.signupText}>Sign up</Text>
+        </TouchableOpacity>
       </View>
       {/* ends */}
     </ImageBackground>
@@ -65,6 +87,7 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center",
     alignItems: "center",
+    height: "100%",
   },
   containerContent: {
     height: "100%",
@@ -78,37 +101,76 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
     color: "#fff",
+    marginBottom: 30,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  icon: {
+    marginRight: 10,
   },
   input: {
-    width: "80%",
     height: 40,
+    width: "70%",
+    padding: 10,
+    color: "white",
     borderColor: "gray",
     borderWidth: 1,
     borderRadius: 5,
-    marginBottom: 15,
-    paddingLeft: 10,
-    color: "#fff",
   },
 
   loginButton: {
     backgroundColor: "#3498db",
     padding: 10,
     borderRadius: 5,
-    width: "60%",
+    width: "50%",
+    marginTop: 30,
+    marginBottom: 30,
   },
   buttonText: {
     color: "white",
     textAlign: "center",
   },
   userExist: {
-    height: 120,
+    flexDirection: "row",
+    height: 130,
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#000",
+    borderColor: "green", // Set the border color for the random View
+    borderWidth: 1,
+    borderRadius: 10,
+    // flex: 1,
   },
   loginText: {
     color: "white",
-    marginBottom: 50,
+    marginBottom: 70,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoLogin: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    marginBottom: 30,
+  },
+  Logo: {
+    height: 110,
+    width: 110,
+    marginBottom: 30,
+    borderRadius: 50,
+  },
+  signupText: {
+    color: "#fff",
+  },
+  signupTextContainer: {
+    marginBottom: 70,
+    marginHorizontal: 5,
   },
 });
--- app/components/auth/register.jsx
+++ app/components/auth/register.jsx
@@ -1,10 +1,209 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Register() {
+//
+const image = require("../../assets/2.jpg");
+//
+export default function Login() {
+  const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Add your authentication logic here
+    console.log("Login button pressed");
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <ImageBackground source={image} style={styles.container}>
+      <View style={styles.containerContent}>
+        {/* ########  logo section */}
+        <View style={styles.logoLogin}></View>
+        <Image
+          style={styles.Logo}
+          source={{
+            uri: "https://res.cloudinary.com/jaytech/image/upload/v1704910436/Beauty_App_prwcqb.png",
+          }}
+        />
+        {/* ends */}
+        <Text style={styles.heading}>Create an account</Text>
+        {/* Username input with icon */}
+        <View style={styles.inputContainer}>
+          <Icon name="user" size={20} color="gold" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            placeholderTextColor="gold"
+            onChangeText={(text) => setUsername(text)}
+            value={username}
+          />
+        </View>
+        {/* Password input with icon */}
+        <View style={styles.inputContainer}>
+          <Icon name="lock" size={20} color="gold" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="gold"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Icon name="lock" size={20} color="gold" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="gold"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Icon name="lock" size={20} color="gold" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="gold"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Icon name="lock" size={20} color="gold" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="gold"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+        </View>
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      {/* #################### user exist */}
+      <View style={styles.userExist}>
+        <Text style={styles.loginText}>Dont have an account?</Text>
+        <TouchableOpacity
+          style={styles.signupTextContainer}
+          onPress={() => navigation.navigate("register")}
+        >
+          <Text style={styles.signupText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+      {/* ends */}
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  containerContent: {
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.6)",
+  },
+
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: "#fff",
+    marginBottom: 30,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    height: 40,
+    width: "70%",
+    padding: 10,
+    color: "white",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+
+  loginButton: {
+    backgroundColor: "#3498db",
+    padding: 10,
+    borderRadius: 5,
+    width: "50%",
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  userExist: {
+    flexDirection: "row",
+    height: 130,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+    borderColor: "green", // Set the border color for the random View
+    borderWidth: 1,
+    borderRadius: 10,
+    // flex: 1,
+  },
+  loginText: {
+    color: "white",
+    marginBottom: 70,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoLogin: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    marginBottom: 30,
+  },
+  Logo: {
+    height: 110,
+    width: 110,
+    marginBottom: 30,
+    borderRadius: 50,
+  },
+  signupText: {
+    color: "#fff",
+  },
+  signupTextContainer: {
+    marginBottom: 70,
+    marginHorizontal: 5,
+  },
+});
