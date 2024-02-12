ab5a9da67df260764bb25a444f1b87ca28bb74b7
jayson7 <lexxiijoo70@gmail.com>
2024-02-12 02:55:02 +0100

    jwt config

--- backend/authentications/urls.py
+++ backend/authentications/urls.py
@@ -2,10 +2,13 @@
 
 from django.urls import path, include
 from .views import *
+from rest_framework_simplejwt import views as jwt_views
+
 urlpatterns = [
 
   path('register', registerUsers, name='register'),
   
-
+  path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+  path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh')
     
 ]
--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,  permission_classes
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticated
+from .models import *# Import your Product model
 
 
 # Create your views here.
 
 # hompage. showcase all featured product and trending products
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def HomeView(request):
-    return None
+
+        # Retrieve featured and trending products from your database
+        # featured_products = Product.objects.filter(is_featured=True)
+        # trending_products = Product.objects.filter(is_trending=True)
+
+        # Serialize the products data if you have serializers
+        # featured_serializer_data = YourProductSerializer(featured_products, many=True).data
+        # trending_serializer_data = YourProductSerializer(trending_products, many=True).data
+
+        # Prepare the response content
+        content = {
+            'message': 'Welcome to the JWT Authentication page using React Js and Django!',
+            # 'featured_products': featured_serializer_data,
+            # 'trending_products': trending_serializer_data
+        }
+
+        return Response(content)
+
+    
