018efa0566e909d14517f9b81d8566188d56f5eb
jayson7 <lexxiijoo70@gmail.com>
2024-02-19 22:31:04 +0100

    editing

--- backend/authentications/views.py
+++ backend/authentications/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from rest_framework.decorators import api_view,  permission_classes
+
 
 # Create your views here.
 
-
+# register users
 def registerUsers(request):
     pass
+
+
+# logout
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def Logout(request):
+    
+    try:       
+        refresh_token = request.data["refresh_token"]               
+        token = RefreshToken(refresh_token)               
+        token.blacklist()               
+        return Response(status=status.HTTP_205_RESET_CONTENT)          
+    except Exception as e:         
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+  
