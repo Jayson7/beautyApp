a1196205f9dc72b0c75a34e15b4be77dd4843749
jayson7 <lexxiijoo70@gmail.com>
2024-04-08 15:01:21 +0100

    redux wahala everywhere for like 48 hours now

--- app/components/store/store.js
+++ app/components/store/store.js
@@ -1,15 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import rootReducer from "../reducers/combine/combineReducers"; // Your root reducer
+import { createStore, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "../reducers/combine/combineReducers";
 
-// Import composeWithDevTools from the correct package
-import { composeWithDevTools } from "@reduxjs/toolkit";
-
-// Use configureStore function correctly
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: [thunk],
-  enhancers: [composeWithDevTools()], // Enhance the store with DevTools
-});
+const store = createStore(
+  rootReducer,
+  // applyMiddleware(thunk)
+  applyMiddleware(logger)
+);
 
 export default store;
