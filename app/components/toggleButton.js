9a31d8de1ea10458f75a5b4069ea0eeccdd99621
jayson7 <lexxiijoo70@gmail.com>
2024-04-25 14:54:46 +0100

    editing

--- app/components/toggleButton.js
+++ app/components/toggleButton.js
@@ -2,10 +2,17 @@ import * as React from "react";
 import { ToggleButton } from "react-native-paper";
 
 const ToggleButtonMenu = () => {
+  const [status, setStatus] = React.useState("checked");
+
+  const menuTogger = (value) => {
+    setStatus(status === "checked" ? "unchecked" : "checked");
+  };
+
   return (
     <ToggleButton
       icon="menu"
       value="menu"
+      status={status}
       style={{ backgroundColor: "transparent" }}
       onPress={menuTogger}
     />
