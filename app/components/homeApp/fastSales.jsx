4f2265ca22c3019da13226ce52c7a87b4dd3b480
jayson7 <lexxiijoo70@gmail.com>
2024-05-08 19:30:34 +0100

    homescreen design

--- app/components/homeApp/fastSales.jsx
+++ app/components/homeApp/fastSales.jsx
@@ -1,90 +1,35 @@
 import React from "react";
-import {
-  StyleSheet,
-  FlatList,
-  ImageBackground,
-  View,
-  Text,
-} from "react-native";
-import { Button } from "react-native-paper";
-//
+import { StyleSheet, ImageBackground, View, Text } from "react-native";
 
-const data = [
-  { id: "1", name: "Item 1" },
-  { id: "2", name: "Item 2" },
-  { id: "3", name: "Item 3" },
-  { id: "4", name: "Item 4" },
-  // Add more items as needed
-];
-
-const renderItem = ({ item }) => (
-  <View style={styles.productBox}>
-    <Text>{item.name}</Text>
-  </View>
-);
 const FastSales = () => {
   return (
-    <ImageBackground
-      source={require("../../assets/blob.svg")} // Replace 'path/to/your/image.jpg' with the actual path to your image
-      style={styles.background}
-    >
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={{ fontSize: 30, fontFamily: "ubuntu-italic-bold" }}>
-            Fast Sales
-          </Text>
-        </View>
-        {/*  */}
-        <View>
-          <View style={styles.containerFastSales}>
-            <FlatList
-              data={data}
-              renderItem={renderItem}
-              keyExtractor={(item) => item.id}
-              horizontal={false}
-              showsVerticalScrollIndicator={false} // Remove scroll indicator
-            />
-          </View>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "ubuntu-italic-bold",
+            color: "#566246",
+            backgroundColor: "#fff",
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            borderRadius: 10,
+          }}
+        >
+          Fast Sales
+        </Text>
       </View>
-    </ImageBackground>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    backgroundColor: "red",
-  },
   container: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-
-  containerFeaturedProd: {
-    paddingHorizontal: 5,
-  },
-  headerTxt: {
-    fontSize: 30,
-    fontFamily: "ubuntu-italic-bold",
-    color: "black",
+    marginTop: 20,
     marginBottom: 20,
-    textAlign: "center",
-  },
-  productBox: {
-    padding: 10,
-    margin: 10,
-
-    backgroundColor: "#D8DAD3",
-    borderRadius: 5,
-    width: 170,
-    height: 230,
-    borderRadius: 30,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
 
--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     price = models.CharField(max_length=100)
     discount = models.IntegerField()
     in_stock = models.IntegerField()
-    
+    fast_sales = models.BooleanField(default=False)
     def __str__(self):
         return self.name
     
